0a53b0683daddedc84ab9013d83e697c9d105696
Ginpei <ginpei@gmail.com>
2021-01-30 15:43:22 -0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// $ npm i -D @ginpei/eslintrc
+// $ npm i -D @typescript-eslint/eslint-plugin eslint eslint-config-prettier eslint-plugin-import eslint-plugin-prettier prettier
+
+module.exports = {
+  extends: "./node_modules/@ginpei/eslintrc/.eslintrc.js",
+  rules: {
+    // your rules here
+  },
+};
