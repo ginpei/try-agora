bd6f1fcd99bb58330b025f6139ab34970021fa56
ginpei <ginpei@gmail.com>
2021-01-30 22:28:28 -0800

    feat: 🎸 update secrets example

--- public/secrets.example.js
+++ public/secrets.example.js
@@ -1,1 +1,9 @@
-export const appId = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+// Project Management > Edit > Basic Info > App ID
+// https://console.agora.io/projects
+export const appId = "";
+
+// You can name this
+export const channel = "demo_channel_name";
+
+// Project Management > Edit > Features > Generate temp token
+export const token = "";
